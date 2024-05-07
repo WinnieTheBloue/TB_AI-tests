7417fbe5e8b9c6b64edcc84c6d7ee85b984d191f
WinnieTheBloue <pablo.crotti@heig-vd.ch>
2024-05-07 12:48:25 +0200

    mistral

--- src/App.vue
+++ src/App.vue
@@ -1,155 +1,114 @@
 <script>
-
-import MistralClient from "@mistralai/mistralai";
-import { askEmergencyPlan } from "./utils/openAI";
-import { ref } from "vue";
+import openAIUtils from "./utils/openAI";
+import mistralUtils from "./utils/mistral";
 
 export default {
+  data() {
+    return {
+      isLoading: false,
+      companyInfos: {
+        employees: 20,
+        country: "Suisse",
+      },
+      risk: {
+        name: "Incendie",
+        description: "Risque d'incendie",
+        probability: 1,
+        impact: 4,
+      },
+      answer: null,
+      error: false,
+    };
+  },
 
-    data() {
-        return {
-            isLoading: false,
-            companyInfos: {
-                employees: 20,
-                country: "Suisse",
-            },
-            risk: {
-                name: "Incendie",
-                description: "Risque d'incendie",
-                probability: 1,
-                impact: 4,
-            },
-            openAI: import.meta.env.VITE_OPENAI_API_KEY,
-            mistral: import.meta.env.VITE_MISTRAL_API_KEY,
-            openAiAnswer: null,
+  methods: {
+    async askAI(ai) {
+      this.isLoading = true;
+      try {
+        if (ai == "openAI") {
+          const response = await openAIUtils.askEmergencyPlan(
+            this.risk,
+            this.companyInfos
+          );
 
-            openAiError: false,
-            mistralAnswer: null,
-            mistralError: false,
-        };
+          this.error = false;
+          this.answer = response;
+          this.isLoading = false;
+        } else if (ai == "mistral") {
+          const response = await mistralUtils.askEmergencyPlan(
+            this.risk,
+            this.companyInfos
+          );
+
+          this.error = false;
+          this.answer = response;
+          this.isLoading = false;
+        } else {
+          this.error = "AI not found";
+          this.isLoading = false;
+        }
+      } catch (error) {
+        this.error = error;
+        this.isLoading = false;
+      }
     },
+  },
 
-    methods: {
-        async askOpenAI() {
-            this.isLoading = true;
-            try {
-                const response = await askEmergencyPlan(
-                    this.risk,
-                    this.companyInfos
-                );
-                this.openAiError = false;
-                this.openAiAnswer = response;
-                this.isLoading = false;
-            } catch (error) {
-                this.openAiError = error;
-                this.isLoading = false;
-            }
-        },
-        async askMistral() {
-            this.isLoading = true;
-            const mistral = new MistralClient(this.mistral);
-
-            try {
-                const response = await mistral.chat({
-                    model: "mistral-large-latest",
-                    response_format: { type: "json_object" },
-                    messages: [
-                        {
-                            role: "system",
-                            content: `Tu es spécialisé dans la gestion de risques pour une société. En cas de problèmes, tu peux compter sur ${this.companyInfos.employees} employés pour t'aider. Tu es paramétré pour répondre en JSON et en français.`,
-                        },
-                        {
-                            role: "user",
-                            content: `Établis le plan d'urgence pour ce risque : ${this.risk.description}. La probabilité est estimée à ${this.risk.probability}/4 et l'impact à ${this.risk.impact}/4. Le plan d'urgence doit contenir UNIQUEMENT les informations qu'il faut obtenir pour donner une image claire de la situation si ce risque s'avère et les tâches qu'il faut accomplir pour le surmonter format {informations : [{titre: "Information 1"},{titre : "Information 2"},{titre: "..."},],taches : [{titre: "Tâche 1"},{titre : "Tâche 2"},{titre: "..."},]}. Les tâches et les informations doivent être claires et utiles.`,
-                        },
-                    ],
-                });
-                console.log(response.choices[0].message.content);
-                this.mistralError = false;
-                this.mistralAnswer = JSON.parse(
-                    response.choices[0].message.content
-                );
-                this.isLoading = false;
-            } catch (error) {
-                console.error(error);
-                this.mistralError = true;
-                this.isLoading = false;
-            }
-        },
-    },
-
-    mounted() {
-    },
+  mounted() {},
 };
 </script>
 
 <template>
+  <div
+    v-if="isLoading"
+    class="w-screen h-screen bg-[#ffffff77] fixed flex justify-center items-center gap-5"
+  >
+    <span class="sr-only">Loading...</span>
     <div
-        v-if="isLoading"
-        class="w-screen h-screen bg-[#ffffff77] fixed flex justify-center items-center gap-5"
-    >
-        <span class="sr-only">Loading...</span>
-        <div
-            class="h-8 w-8 bg-blue-700 rounded-full animate-bounce [animation-delay:-0.3s]"
-        ></div>
-        <div
-            class="h-8 w-8 bg-blue-700 rounded-full animate-bounce [animation-delay:-0.15s]"
-        ></div>
-        <div class="h-8 w-8 bg-blue-700 rounded-full animate-bounce"></div>
-    </div>
+      class="h-8 w-8 bg-blue-700 rounded-full animate-bounce [animation-delay:-0.3s]"
+    ></div>
     <div
-        class="flex flex-col justify-center items-center flex-wrap h-screen w-full"
-    >
-        <button
-            @click="askOpenAI"
-            class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
-        >
-            Ask OpenAI
-        </button>
-        <p v-if="openAiError">{{ openAiError }}</p>
-        <div v-if="!openAiError && openAiAnswer">
-            <div v-if="openAiAnswer.informations">
-                <p>Informations :</p>
-                <ul>
-                    <li v-for="information in openAiAnswer.informations">
-                        {{ information.titre }}
-                    </li>
-                </ul>
-            </div>
-            <div v-if="openAiAnswer.informations">
-                <p>Tâches :</p>
-                <ul>
-                    <li v-for="tache in openAiAnswer.taches">
-                        {{ tache.titre }}
-                    </li>
-                </ul>
-            </div>
-        </div>
+      class="h-8 w-8 bg-blue-700 rounded-full animate-bounce [animation-delay:-0.15s]"
+    ></div>
+    <div class="h-8 w-8 bg-blue-700 rounded-full animate-bounce"></div>
+  </div>
+  <div
+    class="flex flex-col justify-center items-center flex-wrap h-screen w-full"
+  >
+    <div>
+      <button
+        @click="askAI('openAI')"
+        class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+      >
+        Ask OpenAI
+      </button>
 
-        <button
-            @click="askMistral"
-            class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
-        >
-            Ask Mistral
-        </button>
-        <p v-if="mistralError">Une erreure avec Mistral est survenue</p>
-        <div v-if="!mistralError && mistralAnswer">
-            <div v-if="mistralAnswer.informations">
-                <p>Informations :</p>
-                <ul>
-                    <li v-for="information in mistralAnswer.informations">
-                        {{ information.titre }}
-                    </li>
-                </ul>
-            </div>
-            <div v-if="mistralAnswer.informations">
-                <p>Tâches :</p>
-                <ul>
-                    <li v-for="tache in mistralAnswer.taches">
-                        {{ tache.titre }}
-                    </li>
-                </ul>
-            </div>
-        </div>
+      <button
+        @click="askAI('mistral')"
+        class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+      >
+        Ask Mistral
+      </button>
     </div>
+
+    <p v-if="error">{{ error }}</p>
+    <div v-if="!error && answer" class="flex justify-center gap-2 mt-2 ">
+      <div v-if="answer.informations" class="border-4 p-4 w-2/5">
+        <h2 class="text-xl text-center mb-1">Informations</h2>
+        <ul class="text-center">
+          <li v-for="information in answer.informations">
+            {{ information.titre }}
+          </li>
+        </ul>
+      </div>
+      <div v-if="answer.informations" class="border-4 p-4 w-2/5">
+        <h2 class="text-xl text-center mb-1">Tâches</h2>
+        <ul class="text-center">
+          <li v-for="tache in answer.taches">
+            {{ tache.titre }}
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
 </template>
--- src/utils/mistral.js
+++ src/utils/mistral.js
@@ -1,17 +1,16 @@
-import OpenAI from "openai";
+import MistralClient from "@mistralai/mistralai";
 
-const openai = new OpenAI({
-    apiKey: import.meta.env.VITE_OPENAI_API_KEY,
-    dangerouslyAllowBrowser: true,
-});
+const mistral = new MistralClient(import.meta.env.VITE_MISTRAL_API_KEY);
 
-const askOpenAI = async (message) => {
+const askMistral = async (message) => {
     try {
-        const completion = await openai.chat.completions.create({
+        const completion = await mistral.chat({
+            model: "mistral-large-latest",
             response_format: { type: "json_object" },
             messages: message,
-            model: "gpt-3.5-turbo",
+
         });
+
         return JSON.parse(completion.choices[0].message.content);
     } catch (error) {
         throw new Error("Une erreuer est survenue, veuillez réessayer plus tard.");
@@ -29,7 +28,8 @@ const askEmergencyPlan = async (risk, companyInfos) => {
             content: `Établis le plan d'urgence pour ce risque : ${risk.description}. La probabilité est estimée à ${risk.probability}/4 et l'impact à ${risk.impact}/4. Le plan d'urgence doit contenir UNIQUEMENT les informations qu'il faut obtenir pour donner une image claire de la situation si ce risque s'avère et les tâches qu'il faut accomplir pour le surmonter format {informations : [{titre: "Information 1"},{titre : "Information 2"},{titre: "..."},],taches : [{titre: "Tâche 1"},{titre : "Tâche 2"},{titre: "..."},]}. Les tâches et les informations doivent être claires et utiles.`,
         },
     ];
-    return await askOpenAI(message);
+    return await askMistral(message);
 }
 
 export default { askEmergencyPlan }
+
